1a35697de37f7b351f361ad064c13c20c42131ed
muzhig <arseniy@potapov.dev>
2024-12-02 13:01:43 +0500

    fix

--- index.py
+++ index.py
@@ -1,12 +1,15 @@
 import json
 import requests
 
+
 def handler(event, context):
     resp = json.dumps({
         'event': event,
-        'context': context,
     })
     return {
         'statusCode': 200,
         'body': resp,
+        'headers': {
+            'Content-Type': 'application/json',
+        },
     }
